6b8b0e7d3cdfec49e00e5764a0f8833eaccdcbdf
Altkern <31113513+Merkosas@users.noreply.github.com>
2024-09-12 19:00:36 -0300

    Update Player.mjs

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,36 @@
 class Player {
-  constructor({x, y, score, id}) {
-
+  constructor({ x, y, score, id }) {
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
   }
 
   movePlayer(dir, speed) {
-
+    switch (dir) {
+      case 'up':
+        this.y -= speed;
+        break;
+      case 'down':
+        this.y += speed;
+        break;
+      case 'left':
+        this.x -= speed;
+        break;
+      case 'right':
+        this.x += speed;
+        break;
+    }
   }
 
   collision(item) {
-
+    return this.x === item.x && this.y === item.y;
   }
 
   calculateRank(arr) {
-
+    const sortedPlayers = arr.sort((a, b) => b.score - a.score);
+    const rank = sortedPlayers.findIndex(player => player.id === this.id) + 1;
+    return `Rank: ${rank}/${arr.length}`;
   }
 }
 
